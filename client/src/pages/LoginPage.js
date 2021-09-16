c3fcd14b7cdead6f259c499982bee727f220d620
Ryno <rjrfourie@outlook.com>
2021-09-16 22:17:27 +0200

    Revert "Added functionality to login page"
    This reverts commit 1601e01021322a7a7d7fbf61e9d426e85a0a73d6.

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,80 +1,47 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import "../assets/css/login.css";
 
 const LoginPage = () => {
 	const [email, setEmail] = useState("");
-	const [password, setPassword] = useState({
-		values: "",
-		showPassword: false,
-	});
+	const [password, setPassword] = useState("");
 
-	const [errorEmail, setErrorEmail] = useState("");
-	const [errorPassword, setErrorPassword] = useState("");
-
-	function visibility() {
-		return console.log("yes");
-	}
-
-    function validate(event) {
-		event.preventDefault();
-
-		if(email.includes("@uwc.ac.za") || email.includes("@myuwc.ac.za") && email.length > 3 && email.toLowerCase()) {
-			setErrorEmail("");
-			setEmail("");
-			console.log(email);
-		}else {
-			setErrorEmail(`Email must contain "@uwc.ac.za" or "@myuwc.ac.za" and must be 8 letters long`);
-		};
-
-		if(password.length > 7 && password !== 12345678 ) {
-			setErrorPassword("");
-			setPassword("");
-			console.log(password);
-		}else {
-			setErrorPassword("Password must be 8 numbers long");
-		};
+    function validate() {
+        console.log(email + password);
     }
 	return (
-		<form className="container">
+		<div className="container">
 			<div className="row justify-content-center">
-				<div className="col-md-5 shadow-lg p-3 mb-5 bg-white rounded">
-					{/* <img src="./"/> */}
+				<div className="col-md-6 shadow-lg p-3 mb-5 bg-white rounded">
 					<h1>Welcome back! Login to get started.</h1>
 					<div className="form-group">
 						<label htmlFor="email" className="text-info">Email</label><br />
 						<input
 							type="text"
 							value={email}
-							placeholder="Enter Email"
-							name="email"
+							placeholder="Enter Username"
+							name="username"
 							className="form-control"
 							onChange={(e) => setEmail(e.target.value)}
 							required
 						/>
-						<p>{errorEmail}</p>
 					</div>
 					<div className="form-group">
-						<input className="text-info">Password</input><br />
+						<label htmlFor="password" className="text-info">Password</label><br />
 						<input
 							type="password"
 							value={password}
 							placeholder="Enter Password"
 							name="password"
 							className="form-control"
-
 							onChange={(e) => setPassword(e.target.value)}
 							required
 						/>
-						<p>{errorPassword}</p>
 					</div>
-					<button type="submit" id="login" className="form-control"  onClick={validate}>
+					<button type="submit" id="login" onClick={validate}>
 						Login
-					</button> <br />
-					<Link to="/signup" id="link-signup">Register</Link>
+					</button>
 				</div>
 			</div>
-		</form>
+		</div>
 	);
 };
 
