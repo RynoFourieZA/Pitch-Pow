ba6256ce7eab74450be484b4b5969722f4ab383a
Ryno <rjrfourie@outlook.com>
2021-09-20 16:42:45 +0200

    Revert "Login and Signup Page Completed"
    This reverts commit bf357d06f172da2c61a2af67da07605e49b33067.

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+<<<<<<< HEAD
 
 const LoginPage = () => {
 	const [email, setEmail] = useState("");
@@ -11,27 +12,83 @@ const LoginPage = () => {
 		<div className="container">
 			<div className="row justify-content-center">
 				<div className="col-md-6 shadow-lg p-3 mb-5 bg-white rounded">
+=======
+import { Link } from "react-router-dom";
+import "../assets/css/login.css";
+
+const LoginPage = () => {
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState({
+		values: "",
+		showPassword: false,
+	});
+
+	const [errorEmail, setErrorEmail] = useState("");
+	const [errorPassword, setErrorPassword] = useState("");
+
+	function visibility() {
+		return console.log("yes");
+	}
+
+    function validate(event) {
+		event.preventDefault();
+
+		if(email.includes("@uwc.ac.za") || email.includes("@myuwc.ac.za") && email.length > 3 && email.toLowerCase()) {
+			setErrorEmail("");
+			setEmail("");
+			console.log(email);
+		}else {
+			setErrorEmail(`Email must contain "@uwc.ac.za" or "@myuwc.ac.za" and must be 8 letters long`);
+		};
+
+		if(password.length > 7 && password !== 12345678 ) {
+			setErrorPassword("");
+			setPassword("");
+			console.log(password);
+		}else {
+			setErrorPassword("Password must be 8 numbers long");
+		};
+    }
+	return (
+		<form className="container">
+			<div className="row justify-content-center">
+				<div className="col-md-5 shadow-lg p-3 mb-5 bg-white rounded">
+					{/* <img src="./"/> */}
+>>>>>>> parent of bf357d0 (Login and Signup Page Completed)
 					<h1>Welcome back! Login to get started.</h1>
 					<div className="form-group">
 						<label htmlFor="email" className="text-info">Email</label><br />
 						<input
 							type="text"
 							value={email}
+<<<<<<< HEAD
 							placeholder="Enter Username"
 							name="username"
+=======
+							placeholder="Enter Email"
+							name="email"
+>>>>>>> parent of bf357d0 (Login and Signup Page Completed)
 							className="form-control"
 							onChange={(e) => setEmail(e.target.value)}
 							required
 						/>
+<<<<<<< HEAD
 					</div>
 					<div className="form-group">
 						<label htmlFor="password" className="text-info">Password</label><br />
+=======
+						<p>{errorEmail}</p>
+					</div>
+					<div className="form-group">
+						<input className="text-info">Password</input><br />
+>>>>>>> parent of bf357d0 (Login and Signup Page Completed)
 						<input
 							type="password"
 							value={password}
 							placeholder="Enter Password"
 							name="password"
 							className="form-control"
+<<<<<<< HEAD
 							onChange={(e) => setPassword(e.target.value)}
 							required
 						/>
@@ -42,6 +99,21 @@ const LoginPage = () => {
 				</div>
 			</div>
 		</div>
+=======
+
+							onChange={(e) => setPassword(e.target.value)}
+							required
+						/>
+						<p>{errorPassword}</p>
+					</div>
+					<button type="submit" id="login" className="form-control"  onClick={validate}>
+						Login
+					</button> <br />
+					<Link to="/signup" id="link-signup">Register</Link>
+				</div>
+			</div>
+		</form>
+>>>>>>> parent of bf357d0 (Login and Signup Page Completed)
 	);
 };
 
--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -18,12 +18,46 @@ const SignUp = () => (
                     </div>
                 </div>
 
+<<<<<<< HEAD
                 <div className="col-sm-12 col-md-12 col-lg-8 col-xl-8">
                     <div>
                         <form>
                             <input type="text" id="fname" name="fname" />
                             <input type="text" id="lname" name="lname" />
                         </form>
+=======
+            <div className="col-sm-12 col-md-12 col-lg-8 col-xl-8">
+                <AuthHeader />
+                <div className="d-flex justify-content-center">
+                    <div>
+                        <div className="text-center">
+                            <img src={ceiLogo} alt="cei-logo" width="65px"/>
+                            <p className="pt-3">Welcome! Sign up to get started.</p>
+                        </div>
+
+                        <form className="d-flex flex-column">
+                            <p className="mt-3 mb-0">Full Name</p>
+                            <input className="input" type="text" id="full_name" name="full_name" />
+
+                            <p className="mt-3 mb-0">Email</p>
+                            <input className="input"  type="text" id="email" name="email" />
+
+                            <p className="mt-3 mb-1">Role</p>
+                            <div>
+                                <select name="role" id="cars" className="input">
+                                    <option value="Student">Student</option>
+                                    <option value="Mentor">Mentor</option>
+                                </select>
+                            </div>
+
+                            <p className="mt-3 mb-0">Password</p>
+                            <input className="input"  type="password" id="email" name="password" />
+
+                            <div className="my-2">
+                                <YellowButton text={"Sign up"}/>
+                            </div>
+                        </form> 
+>>>>>>> parent of bf357d0 (Login and Signup Page Completed)
                     </div>
                 </div>
             </div>
