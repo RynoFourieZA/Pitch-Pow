0ca70373e12285b2bc4232feab03b0ff90af47ca
Shawen Aldred Harker <harkershawen@gmail.com>
2021-09-25 15:30:06 +0200

    Pitch-pow-remote
    This is the new and current database for pitch-pow

--- table_creation_v3.sql
+++ table_creation_v3.sql
@@ -12,23 +12,23 @@ CREATE TABLE users (
 	email varchar(50) NOT NULL,
 	password varchar(255) NOT NULL,
 	student_number int,
-	confirm varchar(10) NOT NULL DEFAULT false,
+	confirm boolean NOT NULL DEFAULT false,
 	biography varchar(250),
 	profile_picture varchar(600),
 	create_date date not null default CURRENT_DATE,
 	modifyby varchar(50),
 	modify_date date not null default CURRENT_DATE,
-	isDelete boolean NOT NULL DEFAULT false       
+	is_delete boolean NOT NULL DEFAULT false       
 );
 
 CREATE TABLE  category_type (
 	id SERIAL PRIMARY KEY,
-	name varchar(250),
-	description varchar(250),
+	name varchar(150),
+	description varchar(150),
 	create_date date not null default CURRENT_DATE,
 	modifyby uuid REFERENCES users(id),
 	modify_date date not null default CURRENT_DATE,
-	isDelete boolean NOT NULL DEFAULT false
+	is_delete boolean NOT NULL DEFAULT false
 );
 
 CREATE TABLE pitch_type(
@@ -37,29 +37,18 @@ CREATE TABLE pitch_type(
 	create_date date not null default CURRENT_DATE,
 	modifyby uuid REFERENCES users(id),
 	modify_date date not null default CURRENT_DATE,
-	isDelete boolean NOT NULL DEFAULT false
+	is_delete boolean NOT NULL DEFAULT false
 );
 
 CREATE TABLE questions (
 	id SERIAL PRIMARY KEY,
-	questions varchar(350) NOT NULL,
+	questions varchar(250) NOT NULL,
 	category_type_id int REFERENCES category_type(id),
 	pitch_type_id int REFERENCES pitch_type(id),
 	create_date date not null default CURRENT_DATE,
 	modifyby uuid REFERENCES users(id),
 	modify_date date not null default CURRENT_DATE,
-	isDelete boolean NOT NULL DEFAULT false
-);
-
-CREATE TABLE answers (
-	id SERIAL PRIMARY KEY,
-	answered boolean NOT NULL DEFAULT false,
-    question_id int REFERENCES questions(id),
-    student_number int NOT NULL,
-	create_date date not null default CURRENT_DATE,
-	modifyby uuid REFERENCES users(id),
-	modify_date date not null default CURRENT_DATE,
-	isDelete boolean DEFAULT false
+	is_delete boolean NOT NULL DEFAULT false
 );
 
 CREATE TABLE comments (
@@ -69,17 +58,29 @@ CREATE TABLE comments (
 	create_date date not null default CURRENT_DATE,
 	modifyby uuid REFERENCES users(id),
 	modify_date date not null default CURRENT_DATE,
-	isDelete boolean NOT NULL DEFAULT false
+	is_delete boolean NOT NULL DEFAULT false
 );
 
 CREATE TABLE pitch (
 	id SERIAL PRIMARY KEY,
     pitch_string varchar(4500),
 	pitch_type_id int REFERENCES pitch_type(id),
-	comment_id int REFERENCES comments(id),
-	user_id uuid REFERENCES users(id),
+	student_number int NOT NULL,
 	create_date date not null default CURRENT_DATE,
 	modifyby uuid REFERENCES users(id),
 	modify_date date not null default CURRENT_DATE,
-	isDelete boolean DEFAULT false
+	is_delete boolean DEFAULT false
+);
+
+CREATE TABLE answers (
+	id SERIAL PRIMARY KEY,
+	is_answered boolean NOT NULL DEFAULT false,
+    question_id int REFERENCES questions(id),
+    student_number int NOT NULL,
+	pitch_id int REFERENCES pitch(id),
+	comment_id int REFERENCES comments(id),
+	create_date date not null default CURRENT_DATE,
+	modifyby uuid REFERENCES users(id),
+	modify_date date not null default CURRENT_DATE,
+	is_delete boolean DEFAULT false
 );
