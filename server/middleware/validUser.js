06ba3b272018b781dafc416b723da81b4b0096f7
Shawen Aldred Harker <harkershawen@gmail.com>
2021-09-26 20:20:09 +0200

    Auth completed
    We can create and log in users..

--- server/middleware/validUser.js
+++ server/middleware/validUser.js
@@ -3,11 +3,11 @@ require("dotenv").config();
 
 module.exports = async (req, res, next) => {
     try {
-        const jwtToken = req.header("token")
+        const jwtToken = req.header("token");
 
         if (!jwtToken) {
             return res.status(403).json("Not Authorized");
-        }
+        };
 
         const payload = jwt.verify(jwtToken, process.env.JWT_SECRET);
 
--- server/routes/Authentication.js
+++ server/routes/Authentication.js
@@ -7,53 +7,52 @@ const validUser = require("../middleware/validUser");
 //Signup / Register route
 router.post("/signup", async (req, res) => {
     try {
-        //1. Destructure the req.body {email, password, full name, role}
-        const { full_name, email, password, roles } = req.body;
+			//1. Destructure the req.body {email, password, full name, role}
+			const { full_name, email, password, roles } = req.body;
 
-        //2. Check if user exists (if user exists throw error)
-        //Check if student or mentor exists
-        if (roles === "student"){
-            const user = await connectDb.query("SELECT * FROM students WHERE  email = $1", [email]);
-            if(user.rows.length !== 0){
-                return res.status(401).send("User Already Exists");
-            }
-        } else if (roles === "mentor"){
-            const user = await connectDb.query("SELECT * FROM mentors WHERE  email = $1", [email]);
-            if(user.rows.length !== 0){
-                return res.status(401).send("User Already Exists");
-            }
-        }
+			//2. Check if user exists (if user exists throw error)
+			const user = await connectDb.query(
+				"SELECT * FROM users WHERE email = $1",
+				[email]
+			);
 
-        //3. Bcrypt the user password
-        const saltRounds = 10;
-        const salt = await bcrypt.genSalt(saltRounds);
-        const bcryptPassword = await bcrypt.hash(password, salt);
-        
-        //Add user to the database (mentor or student)
-        const student_id = email.replace("@myuwc.ac.za", "");
+			if (user.rows.length !== 0) {
+				return res.status(401).send("User Already Exists");
+			}
 
-        if (roles === "student") {
-            const newUser = await connectDb.query(
-                "INSERT INTO students (id, full_name, email, password, roles) VALUES ($1, $2, $3, $4, $5) RETURNING *",
-                [student_id, full_name, email, bcryptPassword, roles]
-            );
+			//3. Bcrypt the user password
+			const saltRounds = 10;
+			const salt = await bcrypt.genSalt(saltRounds);
+			const bcryptPassword = await bcrypt.hash(password, salt);
 
-            //5. Generating our jwt token
-            const token = jwtGenerator(newUser.rows[0].id);
-            return res.json(token);
+			const role = await connectDb.query(
+				"SELECT id FROM role_type WHERE role_name = $1",
+				[roles]
+			);
 
-        } else if (roles === "mentor") {
-            const newUser = await connectDb.query(
-                "INSERT INTO mentors (full_name, email, password, roles) VALUES ($1, $2, $3, $4) RETURNING *",
-                [full_name, email, bcryptPassword, roles]
-            );
+			const roleValue = role.rows[0].id;
 
-            //5. Generating out jwt token
-            const token = jwtGenerator(newUser.rows[0].id);
-            return res.json(token);
-        }
+			if (roleValue === 1) {
+				const student_id = await email.replace("@myuwc.ac.za", "");
 
-    } catch(err) {
+				const newUser = await connectDb.query(
+					"INSERT INTO users (role_type_id, name, email, password, student_number) VALUES ($1, $2, $3, $4, $5) RETURNING *",
+					[roleValue, full_name, email, bcryptPassword, student_id]
+				);
+				//5. Generating our jwt token
+				const token = jwtGenerator(newUser.rows[0].id);
+				return res.json({ token });
+			} else if (roleValue === 2) {
+				const newUser = await connectDb.query(
+					"INSERT INTO users (role_type_id, name, email, password) VALUES ($1, $2, $3, $4) RETURNING *",
+					[roleValue, full_name, email, bcryptPassword]
+				);
+
+				//5. Generating our jwt token
+				const token = jwtGenerator(newUser.rows[0].id);
+				return res.json({ token });
+			}
+		} catch(err) {
         console.error(err.message);
         res.status(500).send("Server Error");
     }
@@ -62,32 +61,17 @@ router.post("/signup", async (req, res) => {
 router.post("/login", async (req, res) => {
     try {
         const { email, password } = req.body;
-        const student = await connectDb.query("SELECT * FROM students WHERE email = $1", [email]);
-        const mentor = await connectDb.query("SELECT * FROM mentors WHERE email = $1", [email]);
 
-        if (student.rows.length === 0 && mentor.rows.length === 0) {
-            return res.status(401).send("User Does Not Exist")
-        }
+        const user = await connectDb.query("SELECT * FROM users WHERE email = $1", [email]);
 
-        if (student.rows.length > 0) {
-            const validStudentPassword = await bcrypt.compare(password, student.rows[0].password);
+		const validStudentPassword = await bcrypt.compare(password, user.rows[0].password);
 
-            if (!validStudentPassword) {
-                return res.status(401).send("User Does Not Exist 1")
-            }
+        if (user.rows.length === 0 || !validStudentPassword) {
+			return res.status(401).send("User Or Password Does Not Exist");
+		}
 
-            const studentToken = jwtGenerator(student.rows[0].id)
-            return res.json({ studentToken });
-        } else if (mentor.rows.length > 0) {
-            const validMentorPassword = await bcrypt.compare(password, mentor.rows[0].password);
-
-            if (!validMentorPassword){
-                return res.status(401).send("User Does Not Exist 2")
-            };
-
-            const mentorToken = jwtGenerator(mentor.rows[0].id)
-            return res.json({ mentorToken });
-        }
+		const token = jwtGenerator(user.rows[0].id);
+		res.json({ token });
 
     } catch (err) {
         console.error(err.message);
