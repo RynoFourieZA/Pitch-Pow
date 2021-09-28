1e1b7b3990bf3dec7c50abc3bc3aa54068968170
Shawen Aldred Harker <harkershawen@gmail.com>
2021-09-28 20:37:55 +0200

    Working Branch on Front-end

--- server/routes/Authentication.js
+++ server/routes/Authentication.js
@@ -51,10 +51,9 @@ router.post("/signup", validInfo, async (req, res) => {
 				const token = jwtGenerator(newUser.rows[0].id);
 				return res.json({ token });
 			}
-		} catch(err) {
+		}catch(err) {
         console.error(err.message);
-
-        res.status(500).send("Server Error");
+		res.status(500).send("Server Error");
     }
 });
 
