44b19b081b021481a9466fdbb3d4fe20c883c389
Ryno <rjrfourie@outlook.com>
2021-10-14 13:54:01 +0200

    got all the information and then config it.
    It needed to work with the accordion feather

--- client/src/components/Q&A/Questions_New.js
+++ client/src/components/Q&A/Questions_New.js
@@ -1,12 +1,8 @@
-import React, { useState, useEffect} from "react";
-import Pagination from "./Pagination";
-// import InputAnswer from "./InputAnswer";
+import React, { useState, useEffect } from "react";
 import Accordion from "./Accordion";
 
 const Questions_New = () => {
     const [data, setData] = useState([]);
-	const [currentPage, setCurrentPage] = useState(1);
-	const [questionPerPage] = useState(2);
 
     async function getNewQuestions() {
 		try {
@@ -16,8 +12,101 @@ const Questions_New = () => {
 			});
 
 			const parseRes = await response.json();
-            const newData = parseRes.filter(item => item.pitch_type_name === "New")
-			setData(newData);
+            const newData = await parseRes.filter(item => item.pitch_type_name === "New");
+			// "Business Description"
+			const getBusinessDescription = newData.filter(questions => questions.name === "Business Description").map(question => {
+				delete question.name;
+				return question;
+			});
+			const propertyAdd = getBusinessDescription.map(item => item);
+			const obj1 = [{
+				"Business Description": propertyAdd.map(item => item)
+			}]
+		
+			// "Innovation"
+			const getInnovation = newData.filter(questions => questions.name === "Innovation").map(question => {
+				delete question.name;
+				return question;
+			});
+			const propertyAddInnovation = getInnovation.map(item => item);
+			const obj2 = [{
+				"Innovation": propertyAddInnovation.map(item => item)
+			}]
+		
+		
+			// Market Analysis
+			const getMA = newData.filter(questions => questions.name === "Market Analysis").map(question => {
+				delete question.name;
+				return question;
+			});
+			const propertyAddMA = getMA.map(item => item);
+			const obj3 = [{
+				"Market Analysis": propertyAddMA.map(item => item)
+			}]
+		
+			// Product or Service Analysis
+			const getPSA = newData.filter(questions => questions.name === "Product or Service Analysis").map(question => {
+				delete question.name;
+				return question;
+			});
+			const propertyAddPSA = getPSA.map(item => item);
+			const obj4 = [{
+				"Product or Service Analysis": propertyAddPSA.map(item => item)
+			}]
+		
+			// Competition
+			const getCompetition = newData.filter(questions => questions.name === "Competition").map(question => {
+				delete question.name;
+				return question;
+			});
+			const propertyAddCompetition = getCompetition.map(item => item);
+			const obj5 = [{
+				"Competition": propertyAddCompetition.map(item => item)
+			}]
+		
+			// Marketing Strategy
+			const getMS = newData.filter(questions => questions.name === "Marketing Strategy").map(question => {
+				delete question.name;
+				return question;
+			});
+			const propertyAddMS = getMS.map(item => item);
+			const obj6 = [{
+				"Marketing Strategy": propertyAddMS.map(item => item)
+			}]
+		
+			// Operations
+			const getOperations = newData.filter(questions => questions.name === "Operations").map(question => {
+				delete question.name;
+				return question;
+			});
+			const propertyAddOperations = getOperations.map(item => item);
+			const obj7 = [{
+				"Operations": propertyAddOperations.map(item => item)
+			}]
+		
+			// Finances
+			const getFinances = newData.filter(questions => questions.name === "Finances").map(question => {
+				delete question.name;
+				return question;
+			});
+			const propertyAddFinances = getFinances.map(item => item);
+			const obj8 = [{
+				"Finances": propertyAddFinances.map(item => item)
+			}]
+		
+			// Management/Technical complexity
+			const getMTc = newData.filter(questions => questions.name === "Management/Technical complexity").map(question => {
+				delete question.name;
+				return question;
+			});
+			const propertyAddMTc = getMTc.map(item => item);
+			const obj9 = [{
+				"Management/Technical complexity": propertyAddMTc.map(item => item)
+			}]
+		
+			const allTheObjects = [...obj1, ...obj2, ...obj3, ...obj4, ...obj5, ...obj6, ...obj7, ...obj8, ...obj9];
+			setData(allTheObjects);
+
 
 		} catch (e) {
 			console.error(e.message);
@@ -28,31 +117,13 @@ const Questions_New = () => {
 		(async () => await getNewQuestions())();
 	}, []);
 
-
-	// Get current questions
-	const indexOfLastQuestion = currentPage * questionPerPage;
-	const indexOfFirstQuestion = indexOfLastQuestion - questionPerPage;
-	const currentQuestions = data.slice(
-		indexOfFirstQuestion,
-		indexOfLastQuestion
-	);
-
-	// Change page
-	const paginate = (pageNumber) => setCurrentPage(pageNumber);
-
     return (
-        <div className="container mt-3s">
-			{data.map((questions) => (
-				<div key={questions.id} className="questions-answer-box" >
-					<Accordion questions={questions}/>
-					{/* <InputAnswer questions={questions}/> */}
-					</div>
+        <div className="container">
+			{data.map((property, index) => (
+				<div key={index} className="questions-answer-box" >
+					<Accordion property={property} />
+				</div>
 			))}
-			<Pagination
-				questionPerPage={questionPerPage}
-				totalQuestions={data.length}
-				paginate={paginate}
-			/>
 		</div>
     )
 }
