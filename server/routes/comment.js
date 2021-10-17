43bc9e38c7ba620943881d6acaaf91901a8f1c1a
Shawen Aldred Harker <harkershawen@gmail.com>
2021-10-17 14:49:48 +0200

    Server side fixed

--- server/routes/comment.js
+++ server/routes/comment.js
@@ -7,19 +7,16 @@ const router = new Router();
 
 router.post("/comments", async (req, res) => {
 	try {
-        const { string, student_no } = req.body;
+        const { string, answer_id } = req.body;
 		const token = req.header("token");
 
 		const user_id = await parseJwt(token).user;
 
 		const user = await connectDb.query("SELECT name, email FROM users WHERE id = $1", [user_id]);
-		const pitch_id = await connectDb.query("SELECT id FROM pitch WHERE student_no = $1", [student_no]);
-
 		const user_name = await user.rows[0].name;
-		const id = await pitch_id.rows[0].id;
 
         connectDb
-            .query("INSERT INTO comments (comment, pitch_id, users_id, created_by, created_date) VALUES ($1, $2, $3, $4, $5)", [string, id, user_id, user_name, date])
+            .query("INSERT INTO comments (comment, answer_id, users_id, created_by, created_date) VALUES ($1, $2, $3, $4, $5)", [string, answer_id, user_id, user_name, date])
             .then(() => res.json("Your comment was submitted."))
 	} catch (e) {
 		console.error(e.message);
@@ -30,7 +27,7 @@ router.post("/comments", async (req, res) => {
 router.get("/comments", async (_, res) => {
 	try {
 		connectDb
-			.query("SELECT pitch_id, comment, created_by FROM comments ORDER BY created_by")
+			.query("SELECT answer_id, comment, created_by FROM comments ORDER BY created_by")
 			.then((result) => res.json(result.rows));
 	} catch (e) {
 		console.error(e.message);
