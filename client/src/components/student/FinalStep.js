80bc7901eb47990d4bad79862319cbf99a9852fe
RynoFourieZA <rjrfourie@outlook.com>
2021-12-08 16:58:51 +0200

    inserting a transition and some confetti if you finish the pitch

--- client/src/components/student/FinalStep.js
+++ client/src/components/student/FinalStep.js
@@ -4,6 +4,21 @@ import "../../assets/css/finalStep.css";
 const FinalStep = () => {
 	return (
 		<main className="center">
+			<div className="confetti">
+				<div className="confetti-piece"></div>
+				<div className="confetti-piece"></div>
+				<div className="confetti-piece"></div>
+				<div className="confetti-piece"></div>
+				<div className="confetti-piece"></div>
+				<div className="confetti-piece"></div>
+				<div className="confetti-piece"></div>
+				<div className="confetti-piece"></div>
+				<div className="confetti-piece"></div>
+				<div className="confetti-piece"></div>
+				<div className="confetti-piece"></div>
+				<div className="confetti-piece"></div>
+				<div className="confetti-piece"></div>
+			</div>
 			<h1 className="congrats">Congratulations</h1>
 			<div className="text">
 				<h2>You have completed your pitch</h2>
